bea109fb5ff2b67572db1f102393b372c4e64463
João Pereira <jpereira@gitlab.com>
2020-05-07 22:17:08 +0000

    Fix comparison of fixture timestamps across different timezones

--- registry/datastore/testdata/fixtures/layers.sql
+++ registry/datastore/testdata/fixtures/layers.sql
@@ -1,9 +1,9 @@
 INSERT INTO "layers"("id","media_type","digest_hex","size","created_at","marked_at","deleted_at")
 VALUES
-(1,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('c9b1b535fdd91a9855fb7f82348177e5f019329a58c53c47272962dd60f71fc9','hex'),2802957,E'2020-03-04 20:05:35.338639',NULL,NULL),
-(2,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('6b0937e234ce911b75630b744fb12836fe01bda5f7db203927edbb1390bc7e21','hex'),108,E'2020-03-04 20:05:35.338639',NULL,NULL),
-(3,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('f01256086224ded321e042e74135d72d5f108089a1cda03ab4820dfc442807c1','hex'),109,E'2020-03-04 20:06:32.856423',NULL,NULL),
-(4,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('68ced04f60ab5c7a5f1d0b0b4e7572c5a4c8cce44866513d30d9df1a15277d6b','hex'),27091819,E'2020-03-04 20:08:00.405042',NULL,NULL),
-(5,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('c4039fd85dccc8e267c98447f8f1b27a402dbb4259d86586f4097acb5e6634af','hex'),23882259,E'2020-03-04 20:08:00.405042',NULL,NULL),
-(6,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('c16ce02d3d6132f7059bf7e9ff6205cbf43e86c538ef981c37598afd27d01efa','hex'),203,E'2020-03-04 20:08:00.405042',NULL,NULL),
-(7,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('a0696058fc76fe6f456289f5611efe5c3411814e686f59f28b2e2069ed9e7d28','hex'),107,E'2020-03-04 20:08:00.405042',NULL,NULL);
+(1,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('c9b1b535fdd91a9855fb7f82348177e5f019329a58c53c47272962dd60f71fc9','hex'),2802957,E'2020-03-04 20:05:35.338639+00',NULL,NULL),
+(2,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('6b0937e234ce911b75630b744fb12836fe01bda5f7db203927edbb1390bc7e21','hex'),108,E'2020-03-04 20:05:35.338639+00',NULL,NULL),
+(3,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('f01256086224ded321e042e74135d72d5f108089a1cda03ab4820dfc442807c1','hex'),109,E'2020-03-04 20:06:32.856423+00',NULL,NULL),
+(4,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('68ced04f60ab5c7a5f1d0b0b4e7572c5a4c8cce44866513d30d9df1a15277d6b','hex'),27091819,E'2020-03-04 20:08:00.405042+00',NULL,NULL),
+(5,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('c4039fd85dccc8e267c98447f8f1b27a402dbb4259d86586f4097acb5e6634af','hex'),23882259,E'2020-03-04 20:08:00.405042+00',NULL,NULL),
+(6,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('c16ce02d3d6132f7059bf7e9ff6205cbf43e86c538ef981c37598afd27d01efa','hex'),203,E'2020-03-04 20:08:00.405042+00',NULL,NULL),
+(7,E'application/vnd.docker.image.rootfs.diff.tar.gzip',decode('a0696058fc76fe6f456289f5611efe5c3411814e686f59f28b2e2069ed9e7d28','hex'),107,E'2020-03-04 20:08:00.405042+00',NULL,NULL);
