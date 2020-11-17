f8e0cc9c5ce02e4b9f479ed2baddf065695c8c7e
João Pereira <jpereira@gitlab.com>
2020-11-17 11:40:30 +0000

    Fix DB constraint names and DDL

--- migrations/20201019155109_create_manifests_table_partitions.go
+++ migrations/20201019155109_create_manifests_table_partitions.go
@@ -3,16 +3,18 @@ package migrations
 import migrate "github.com/rubenv/sql-migrate"
 
 func init() {
-	m := &Migration{Migration: &migrate.Migration{
-		Id: "20201019155109_create_manifests_table_partitions",
-		Up: []string{
-			`CREATE TABLE partitions.manifests_default PARTITION OF public.manifests
-			FOR VALUES WITH (MODULUS 1, REMAINDER 0)`,
+	m := &Migration{
+		Migration: &migrate.Migration{
+			Id: "20201019155109_create_manifests_table_partitions",
+			Up: []string{
+				`CREATE TABLE partitions.manifests_default PARTITION OF public.manifests
+				FOR VALUES WITH (MODULUS 1, REMAINDER 0)`,
+			},
+			Down: []string{
+				"DROP TABLE IF EXISTS partitions.manifests_default CASCADE",
+			},
 		},
-		Down: []string{
-			"DROP TABLE IF EXISTS partitions.manifests_default CASCADE",
-		},
-	}}
+	}
 
 	allMigrations = append(allMigrations, m)
 }
--- migrations/20201019155128_create_manifest_references_table_partitions.go
+++ migrations/20201019155128_create_manifest_references_table_partitions.go
@@ -3,16 +3,18 @@ package migrations
 import migrate "github.com/rubenv/sql-migrate"
 
 func init() {
-	m := &Migration{Migration: &migrate.Migration{
-		Id: "20201019155128_create_manifest_references_table_partitions",
-		Up: []string{
-			`CREATE TABLE partitions.manifest_references_default PARTITION OF public.manifest_references
-			FOR VALUES WITH (MODULUS 1, REMAINDER 0)`,
+	m := &Migration{
+		Migration: &migrate.Migration{
+			Id: "20201019155128_create_manifest_references_table_partitions",
+			Up: []string{
+				`CREATE TABLE partitions.manifest_references_default PARTITION OF public.manifest_references
+				FOR VALUES WITH (MODULUS 1, REMAINDER 0)`,
+			},
+			Down: []string{
+				"DROP TABLE IF EXISTS partitions.manifest_references_default CASCADE",
+			},
 		},
-		Down: []string{
-			"DROP TABLE IF EXISTS partitions.manifest_references_default CASCADE",
-		},
-	}}
+	}
 
 	allMigrations = append(allMigrations, m)
 }
